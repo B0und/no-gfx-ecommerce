b086a3afe066f976aea668c92ed78cb797773094
Unbound <f1arenova1@gmail.com>
2022-03-11 19:17:01 +0300

    add appbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,10 @@ import { useQuery, UseQueryResult } from "react-query";
 import fetchGraphicsCards from "../src/api/fetchGraphicCards";
 import GraphicsCard from "../src/components/GraphicsCard";
 import { IGraphicsResponse } from "../types/GraphicsCard";
+import PrimarySearchAppBar from "../src/components/PrimarySearchAppBar";
 
 const Home: NextPage = () => {
-  const { isLoading, isError, isSuccess, data, error }: UseQueryResult<IGraphicsResponse, Error> = useQuery<
+  const { isLoading, isError, data, error }: UseQueryResult<IGraphicsResponse, Error> = useQuery<
     IGraphicsResponse,
     Error
   >("graphics-cards", fetchGraphicsCards);
@@ -24,52 +25,34 @@ const Home: NextPage = () => {
   if (isError) {
     return <span>Error: {error.message}</span>;
   }
-  // console.log(data);
-
-  // console.log(data.data.data);
 
   return (
-    <Container maxWidth="lg">
-      {/* {data?.map((videoCard) => (
-        <GraphicsCard key={videoCard.id} {...videoCard} />
-      ))} */}
-
-      {/* {isSuccess
-        ? data.data?.map((videoCard) => (
-            <GraphicsCard
-              key={videoCard.id}
-              displayName={videoCard.displayName}
-              price={videoCard.price}
-              videoChipset={videoCard.videoChipset}
-              developer={videoCard.developer}
-              manufacturer={videoCard.manufacturer}
-            />
-          ))
-        : null} */}
-
-      {data?.data?.map((videoCard) => (
-        <GraphicsCard key={videoCard.id} videoCard={videoCard} />
-      ))}
-
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography variant="h4" component="h1" gutterBottom>
-          MUI v5 + Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <>
+      <Container maxWidth="lg">
+        <PrimarySearchAppBar />
+        {data?.data?.map((videoCard) => (
+          <GraphicsCard key={videoCard.id} videoCard={videoCard} />
+        ))}
+        <Box
+          sx={{
+            my: 4,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h4" component="h1" gutterBottom>
+            MUI v5 + Next.js with TypeScript example
+          </Typography>
+          <Link href="/about" color="secondary">
+            Go to the about page
+          </Link>
+          <ProTip />
+          <Copyright />
+        </Box>
+      </Container>
+    </>
   );
 };
 
--- src/api/fetchGraphicCards.ts
+++ src/api/fetchGraphicCards.ts
@@ -6,3 +6,9 @@ const fetchGraphicsCards = async () => {
   return res.json();
 };
 export default fetchGraphicsCards;
+
+// http://localhost:1337/api/graphics-cards?filters[developer][$eq]=nvidia
+
+// http://localhost:1337/api/developers?fields[0]=name
+
+// https://gfx-backend.herokuapp.com/api/products?pagination[page]=1&pagination[pageSize]=5
