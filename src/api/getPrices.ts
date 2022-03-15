0766ce1f138e22bf803d4cf3bb800eb373285b4a
Unbound <f1arenova1@gmail.com>
2022-03-15 20:10:02 +0300

    add method to get prices from api

--- src/api/getPrices.ts
+++ src/api/getPrices.ts
@@ -2,7 +2,7 @@ import axiosConfig from './axiosConfig'
 
 const getPrices = async (): Promise<number[]> => {
   const res = await axiosConfig.get('/products?fields[0]=price')
-  const prices = res.data.data.map((item: Record<string, unknown>) => item.price)
+  const prices = res.data.data.map((item: Record<string, number>) => item.price)
   return prices
 }
 export default getPrices
