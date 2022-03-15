2eea5efc73c67a05c0741eb0031cd4a554e7682c
Unbound <f1arenova1@gmail.com>
2022-03-15 20:09:15 +0300

    add get developers api

--- src/api/getDevelopers.ts
+++ src/api/getDevelopers.ts
@@ -1,8 +1,13 @@
-import { IGraphicsResponse } from '../../types/GraphicsCard'
 import axiosConfig from './axiosConfig'
 
-const getDevelopers = async (): Promise<IGraphicsResponse> => {
-  const res = await axiosConfig.get('/developers')
-  return res.data
+const capitalize = (str: string) =>
+  str.charAt(0).toUpperCase() + str.substring(1)
+
+const getDevelopers = async (): Promise<string[]> => {
+  const res = await axiosConfig.get('/developers?fields[0]=name')
+  const developers = res.data.data.map((item: Record<string, string>) =>
+    capitalize(item.name)
+  )
+  return developers
 }
 export default getDevelopers
