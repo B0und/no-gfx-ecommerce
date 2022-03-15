282f7d7e24dc4b0507e6d2a7f48de6554280c4a9
Unbound <f1arenova1@gmail.com>
2022-03-15 20:10:17 +0300

    add method to get video chipsets from api

---
+++ src/api/getVideoChipsets.ts
@@ -0,0 +1,11 @@
+import axiosConfig from './axiosConfig'
+
+const getVideoChipsets = async (): Promise<string[]> => {
+  const res = await axiosConfig.get('/video-chipsets?fields[0]=name')
+  const videoChipsets = res.data.data.map(
+    (item: Record<string, string>) => item.name
+  )
+  videoChipsets.sort().reverse()
+  return videoChipsets
+}
+export default getVideoChipsets
