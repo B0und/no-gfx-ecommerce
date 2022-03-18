6e2b536e499c04050b3fa59cdaa72c84181e9e52
Unbound <f1arenova1@gmail.com>
2022-03-18 15:19:54 +0300

    add sort header

---
+++ src/components/SortHeader.tsx
@@ -0,0 +1,30 @@
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from '@mui/material'
+import React from 'react'
+
+const typesList = ['popular', 'cheapFirst', 'expensiveFirst', 'discount']
+type SortType = typeof typesList[number]
+
+const SortHeader = () => {
+  const [sortBy, setSortBy] = React.useState<SortType>('popular')
+
+  const handleSortChange = (event: SelectChangeEvent<SortType>) => {
+    setSortBy(event.target.value as SortType)
+  }
+  return (
+    <FormControl>
+      <Select value={sortBy} onChange={handleSortChange}>
+        <MenuItem value="popular">Popular</MenuItem>
+        <MenuItem value="cheapFirst">Cheap First</MenuItem>
+        <MenuItem value="expensiveFirst">Expensive First</MenuItem>
+        <MenuItem value="discount">Discount</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+export default SortHeader
